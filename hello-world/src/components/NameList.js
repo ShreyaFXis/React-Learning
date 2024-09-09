3258cd9a7507454c57d8446ffa9b971d3c5d30bd
Shreya Panchal <shreya.panchal@fxis.ai>
2024-09-09 15:04:11 +0530

    Hello-world Commit 3

---
+++ hello-world/src/components/NameList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Person from './Person.js'
+
+function NameList() {
+  const names = ['Shrevi', 'Dhruv', 'Dhruvish', 'Shrevi', 'Bhavi']
+  const persons = [
+    {
+      id: 1,
+      name: 'Shrevi',
+      age: 20,
+      skill: 'Python'
+    },
+    {
+      id: 2,
+      name: 'Dhruv',
+      age: 17,
+      skill: 'Angular'
+    },
+    {
+      id: 3,
+      name: 'Dhruvish',
+      age: 17,
+      skill: 'Vue'
+    }
+  ]
+  const personList = persons.map(person => <Person key={person.id} person={person} />)
+  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+  return <div>{NameList}</div>
+  
+}
+
+export default NameList
