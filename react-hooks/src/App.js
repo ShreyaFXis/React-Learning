a79e11e95add21541f8202a5bd1b13d499757bbd
Shreya Panchal <shreya.panchal@fxis.ai>
2024-09-10 18:02:06 +0530

    Hooks 2

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import ClassCounter from './Components/ClassCounter';
 import ClassCounterTwo from './Components/ClassCounterTwo';
 import HookCounter from './Components/HookCounter';
+import HookCounterTwo from './Components/HookCounterTwo';
+import HookCounterThree from './Components/HookCounterThree';
+import HookCounterFour from './Components/HookCounterFour';
+import IntervalClassCounter from './Components/IntervalClasscounter';
+import IntervalHookCounter from './Components/IntervalHookCounter';
+import HookMouse from './Components/HookMouse';
 
 function App() {
   return (
+
     <div className="App">
-     {/*<ClassCounter />
-      <ClassCounterTwo/>*/}
+     {/*
+     <ClassCounter />
+      <ClassCounterTwo/>
       <HookCounter/>
+      <HookCounterTwo/>
+      <HookCounterThree/>
+      <HookCounterFour/>
+      <IntervalClassCounter/>
+      <IntervalHookCounter/>
+      */}
+      <HookMouse/>
+
+      
     </div>
   );
 }
